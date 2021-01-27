da644f99ecc8257f16efdf7eef1a9fd4baa113f4
Turki BK <tbkaily.gmail.com>
2021-01-26 16:26:58 -0800

    adding Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,19 @@
-function Home (){
-
-<h1>hello </h1>
-
+ import React from "react"
+ import './Home.css';
+ import HomePageImg from "./HomePageImg"
+ 
+ export default function Home (props){
+ 
+        return (
+        
+        <div className ="wrapper"> 
+        
+        <HomePageImg/>
+        <HomePageImg/>
+        <HomePageImg/>
+        <HomePageImg/>
+        
+       
+        </div>
+        )
 }
