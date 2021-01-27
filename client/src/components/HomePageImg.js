7bc46f64cba0361d69463f4e22ebab9f0007e978
Turki BK <tbkaily.gmail.com>
2021-01-26 16:10:20 -0800

    delet line

---
+++ client/src/components/HomePageImg.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './HomePageImg.css';
+
+
+
+export default function HomePageImg(props) {
+
+    return (
+      <div className="container"> 
+        
+        <div className="img">
+            <img src={"null"} alt="Img" />
+        </div>
+        <div className="h4">
+            <h4 >Item</h4>
+            <h4 >$0.00</h4>
+        </div>
+        <div className="pdown">
+            <h5 >place</h5>
+        
+        </div>
+       </div>
+
+)
+    
+}
