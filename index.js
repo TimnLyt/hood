ca56dfcfa8bfbcd42e570fb9776aea1d0ecab826
Turki BK <tbkaily.gmail.com>
2021-02-01 18:10:53 -0800

    adding index

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require(express)
+const app     = express()
+const port = 4000
+
+
+
+app.use(logger)
+ console.log("home page ")
+app.get('/', (req, res) => {
+  
+})
+
+const logger =( (req, res, next) => {
+  next()
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
