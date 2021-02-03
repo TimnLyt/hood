2d6cefe611578e2cfaee61612db212b0d566ea4d
Turki BK <tbkaily.gmail.com>
2021-02-02 20:18:14 -0800

    I created seallerrothe

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require(express)
+const express = require("express")
 const app     = express()
 const port = 4000
+const setInterval = require ("/seller")
+
+
 app.use(logger)
+app.use("/seller",sellerPageRouters)
+
+
 app.get('/', (req, res) => {
   
 })
 
+
+
 const logger =( (req, res, next) => {
   next()
 })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ routers/sellerPageRouter.js
@@ -0,0 +1,22 @@
+const express = require ("express")
+const selerRouter = express.Router()
+
+
+
+selerRouter.get('/seller', (req, res) => {
+  
+})
+selerRouter.post('/seller', (req, res) => {
+  
+})
+
+selerRouter.get('/seller/:id', (req, res) => {
+  
+})
+
+selerRouter.post('/seller/:id', (req, res) => {
+  
+})
+
+
+module.exports = selerRouter
