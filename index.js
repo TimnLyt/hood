845a0e082dff1191977810145d54b4983b923320
Abenezer T <abety8@outlook.com>
2021-02-02 23:11:09 -0800

    added error handler

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app     = express()
 const port = 4000
 const HomeRouters = require("./routers/home")
-const offersRouter=require('./routers/offers')
+const offersRouters=require('./routers/offers')
 
-
+app.use(express.json());
 app.use(logger)
 app.use("/v0",HomeRouters)
-app.use('/api/v1/offers',offersRouter)
+app.use("/offers",offersRouters)
 
 app.get('/', (req, res) => {
   console.log("home")
@@ -20,6 +20,13 @@ app.get('/', (req, res) => {
   next()
   return
 }
+app.use((req,res,next)=>{
+  res.status(404);
+  res.send({
+      error:'not found'
+  });
+})
+ 
 app.listen(port, () => {
-  console.log("Example app listening at http://localhost:${port}")
+  console.log(`Example app listening at http://localhost:${port}`)
 })
