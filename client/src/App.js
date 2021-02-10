d6d6d905a5a02ccb4f16ed11c574277ecd437f8d
AGT1041 <74869949+AGT1041@users.noreply.github.com>
2021-02-10 02:33:43 -0800

    add routes
    add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,44 @@
+import logo from "./logo.svg";
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import All from './components/All';
+import Imgex from './components/Imgex'
+import Nav from './components/nav'
 export function App() {
   return (
+    <>
     <ChatEngine
       projectID="d2aae693-c3d3-4f9b-a786-94417a988839"
       userName="test1"
       userSecret="123"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
+    <BrowserRouter>
+      <Nav/>
+      <Switch>
+        <Route path="/detail/">
+         
+          <Imgex/><All />
+        </Route>
+        <Route path='/'>
+           
+        </Route>
+        <Route>
+         
+        </Route>
+        <Route>
+           
+        </Route>
+        <Route>
+         
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
+</>
   );
-  }
+}
 
 export default App;
