1ebd9ff2f917fc4b3c08e92dd1a3d2ab7a165de0
Abenezer T <abety8@outlook.com>
2021-02-09 23:06:19 -0800

    a contanier to hold detail

---
+++ client/src/components/Cont.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  Grid,
+  Card,
+  Typography,
+  CardContent,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+  card: {
+    height: "150px",
+    padding: theme.spacing(2)
+  },
+}));
+
+export default function Cont(props) {
+  const classes = useStyles();
+  return (
+    <Grid container justify="flex-start" className=
+    {classes.root} spacing={3} align={'left'}>
+      <Grid item xs={1}></Grid>
+      <Grid item xs={3}>
+         
+            <Typography variant="h4" >
+              Price
+            </Typography>
+            <Typography>
+              <p>pros.products.price</p>
+            </Typography>
+           
+      </Grid>
+      <Grid item xs={5}>
+       
+            <Typography variant="h4">
+              Titile
+            </Typography>
+            <Typography>
+              <p>pros.products.tile</p>
+            </Typography>
+          
+      </Grid>
+      <Grid container item xs={9} align={'left'}>
+      <Grid item xs={1}></Grid>
+      <Grid item xs={9}>
+      <Typography variant="h4">
+          Description</Typography>
+      </Grid>
+        
+      </Grid>
+       
+      </Grid>
+         
+       
+
+
+    
+  )
+    
+}
