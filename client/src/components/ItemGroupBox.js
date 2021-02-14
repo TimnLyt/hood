7f9a85be02a96676c6201a73b28a89502e13aff4
mfern08 <74215044+mfern08@users.noreply.github.com>
2021-02-14 12:46:30 -0800

    recovery for my viewAllItems

---
+++ client/src/components/ItemGroupBox.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ItemBox from './ItemBox';
+import './ItemBoxGroup.css';
+
+export default function ItemBoxGroup(){
+    return(
+        <div className ="wrapper">
+           <ItemBox></ItemBox>
+           <ItemBox></ItemBox>
+           <ItemBox></ItemBox>
+       
+
+
+        </div>
+    )
+}
