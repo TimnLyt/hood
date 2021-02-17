b38c551623a27f34b6652576e3a1857c5320f4d5
Abenezer T <abety8@outlook.com>
2021-02-16 22:04:30 -0800

    added product module and mockproduc to add to database

---
+++ service/data/mockProduct.js
@@ -0,0 +1,8 @@
+exports. products = [
+    { id:1234,Name: "Cheese", price : 2.50,  Location: "Refrigerated foods"},
+    { id:1244,name: "Crisps", price : 3,  Location: "the Snack isle"},
+    {id:1739, name: "Pizza", price : 4,  Location: "Refrigerated foods"},
+    { id:1024,name: "Chocolate", price : 1.50,  Location: "the Snack isle"},
+    { id:4284,name: "Self-raising flour", price : 1.50,  Location: "Home baking"},
+    {id:3839, name: "Ground almonds", price : 3,  Location: "Home baking"}
+  ]
