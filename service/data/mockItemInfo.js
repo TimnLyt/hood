38bec80233c40962a2a40595c8653bc9127b1df5
mfern08 <74215044+mfern08@users.noreply.github.com>
2021-02-16 20:01:23 -0800

    added mock item data

---
+++ service/data/mockItemInfo.js
@@ -0,0 +1,39 @@
+exports.posts = [
+    {
+      id: 1,
+      item: 'chair',
+      price: '$10',
+      location: 'Seattle, WA',
+    },
+    {
+      id: 2,
+      item: 'laptop',
+      price: '$150',
+      location: 'Bellevue, WA',
+    },
+    {
+      id: 3,
+      item: 'picture frame',
+      price: '$5',
+      location: 'Bitterlake, WA',
+    },
+    {
+      id: 4,
+      item: 'table',
+      price: '$30',
+      location: 'Renton, WA',
+    },
+    {
+      id: 5,
+      item: 'bed',
+      price: '$60',
+      location: 'Everett, WA',
+    },
+    {
+       id: 6,
+       item: 'car',
+       price: '$1200',
+       location: 'Lynnwood, WA',
+      },
+    
+  ]
