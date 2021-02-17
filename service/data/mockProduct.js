ce497529be6ab34840ad6022982b36a2babbaea3
Abenezer T <abety8@outlook.com>
2021-02-16 23:47:21 -0800

    added mockproduct

--- service/data/mockProduct.js
+++ service/data/mockProduct.js
@@ -1,8 +1,27 @@
-exports. products = [
-    { id:1234,Name: "Cheese", price : 2.50,  Location: "Refrigerated foods"},
-    { id:1244,name: "Crisps", price : 3,  Location: "the Snack isle"},
-    {id:1739, name: "Pizza", price : 4,  Location: "Refrigerated foods"},
-    { id:1024,name: "Chocolate", price : 1.50,  Location: "the Snack isle"},
-    { id:4284,name: "Self-raising flour", price : 1.50,  Location: "Home baking"},
-    {id:3839, name: "Ground almonds", price : 3,  Location: "Home baking"}
-  ]
+
+  exports.products = [
+    {
+      id:1,  
+      name: "Cheese" ,
+      price : 2.50,
+      Location: "Refrigerated foods" 
+    },
+    {
+        id:  2,
+        name: "water" , 
+        price : 10,  
+        Location: "seattle"
+    },
+    {
+      id:  3,
+      name: "bread",
+      price :  20,
+      Location:"north seattl"
+    },
+    {
+        id:  4,
+        name: "Iphone" , 
+        price : 10,  
+        Location: "lakecity"
+    },
+];
