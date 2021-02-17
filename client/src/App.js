9ad8c9a87cc4a4f920074d29a0682ae17fe1870b
Abenezer T <abety8@outlook.com>
2021-02-16 18:42:33 -0800

    edited pacage

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,20 @@ import Nav from './components/nav'
 import User_profile from './components/user_profile'
 import Home from './components/Home'
 import ItemGroupBox from './components/ItemGroupBox';
+import Dialogs from "./components/Dialogs";
 export function App() {
   return (
     <>
-    <ChatEngine
-      projectID="d2aae693-c3d3-4f9b-a786-94417a988839"
-      userName="test1"
-      userSecret="123"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-    />
+     
     <BrowserRouter>
-      <Nav/>
+       
       <Switch>
-        <Route path="/detail/">
+        <Route path="/creat">
          
-          <Imgex/><All />
-        </Route>
-        <Route path='/'>
-
-        <Route path="/Home" exact component={Home}/>
-        <Route Path="/User_profile" component={User_profile}/>  
            
+          <Dialogs/>
         </Route>
-        <Route Path="/ItemGroupBox" component={ItemGroupBox}/>
          
-        <Route>
-           
-        </Route>
-        <Route>
-         
-        </Route>
       </Switch>
     </BrowserRouter>
 
---
+++ client/src/components/Dialogs.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { className,TextField, classes,makeStyles,Grid, Button}from "@material-ui/core";
+import axios from "axios"
+//import "../app.css";
+//import { db } from "../firebase";
+const useStyles = makeStyles(theme => ({
+  root:{
+      height:300,
+      width: 200,
+      
+       
+  },
+  text:{
+    height:200
+  }
+}))
+
+function Dialogs() {
+  const[input, setInput]=useState({
+    content:""
+  })
+   
+   
+  function handleChange(event){
+     const{name, value}=event.target;
+
+     setInput(prevInput=>{
+       return{
+         ...prevInput,
+            [name]:value
+       }
+     })
+  }
+  function handleClick(event){
+    event.preventDefault();
+    console.log(input)
+    const newMessage={
+      content:input.content
+    }
+     axios.post('http://localhost:5000/v1/details',newMessage)
+  }
+
+  const classes = useStyles();
+   
+  return <Grid container direction='column'  
+    justify="center" alignItems='center' style={{minHeight:"70vh"}}> 
+
+  <form className={classes.root} noValidate autoComplete="off">
+   
+   <TextField className={classes.text}
+          //id="standard-multiline-static"
+          onChange={handleChange}
+          label="Message"
+          multiline
+          rows={13}
+          name='content' 
+          value={input.content}
+          variant="outlined"
+           
+        ></TextField>
+        
+  </form>
+  <Grid item>
+          <Button onClick={handleClick}>Submit</Button>
+        </Grid>
+  </Grid>
+  
+}
+
+export default Dialogs;
