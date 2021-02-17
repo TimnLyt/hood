5dff1ca1b80da3a0fa2cb015e9abf8d73897e702
Abenezer T <abety8@outlook.com>
2021-02-16 22:01:53 -0800

    edited initDb to add products

--- service/data/initDB.js
+++ service/data/initDB.js
@@ -1,9 +1,21 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const mockProducts = require('./mockProduct');
+ 
+const Product = require('../models/mockProduct');
+ 
+
+mockProducts.products.forEach((prod) => {
+    
+    Product.create(prod).catch(err => console.log(err));
+    if(error){
+        console.long("something went wrong");
+}});
+
 const user = process.env.MONGO_USER;
 const password = process.env.MONGO_PASS;
-const mongoDB = ` mongodb+srv://<username>:<password>@cluster0.k6vfw.mongodb.net/MockOfferUp?retryWrites=true&w=majority `;
+const mongoDB = ` mongodb+srv://AGT:crud21@cluster0.k6vfw.mongodb.net/MockOfferUp?retryWrites=true&w=majority`;
 mongoose
   .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(
