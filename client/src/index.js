73c67a6ee89171353f4d57537a87055ee77dd9bd
Abenezer T <abety8@outlook.com>
2021-02-23 21:49:22 -0800

    added authhistory and provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'; 
+import {BrowserRouter}from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import Auth0ProviderWithHistory from "../src/Auth0/auth0-provider-with-history";
+import { CssBaseline,ThemeProvider,createMuiTheme } from '@material-ui/core';
+ 
+ 
 
 ReactDOM.render(
   <React.StrictMode>
