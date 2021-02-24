72a608742ef1778ca46f26b5a20e56176ddea307
dinhthienhcm <dinhthienhcm@gmail.com>
2021-02-23 22:02:16 -0800

    fixed routers and added chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { ChatEngine } from "react-chat-engine";
-import ChatFeed from "./components/ChatFeed";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import All from "./components/All";
 import Imgex from "./components/Imgex";
@@ -9,15 +8,11 @@ import Nav from "./components/nav";
 import User_profile from "./components/user_profile";
 import Home from "./components/Home";
 import ItemGroupBox from "./components/ItemGroupBox";
+import Chat from "./Chat";
+
 export function App() {
   return (
     <>
-      <ChatEngine
-        projectID="d2aae693-c3d3-4f9b-a786-94417a988839"
-        userName="test1"
-        userSecret="123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
       <BrowserRouter>
         <Nav />
         <Switch>
@@ -25,14 +20,14 @@ export function App() {
             <Imgex />
             <All />
           </Route>
+
           <Route path="/">
             <Route path="/Home" exact component={Home} />
-            <Route Path="/User_profile" component={User_profile} />
+            <Route path="/User_profile" component={User_profile} />
+            <Route path="/Chat" component={Chat} />
           </Route>
-          <Route Path="/ItemGroupBox" component={ItemGroupBox} />
 
-          <Route></Route>
-          <Route></Route>
+          <Route path="/ItemGroupBox" component={ItemGroupBox} />
         </Switch>
       </BrowserRouter>
     </>
