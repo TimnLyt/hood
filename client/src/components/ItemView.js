b906df3ef02348b82490cf9afb9f00b6a74af4f2
mfern08 <74215044+mfern08@users.noreply.github.com>
2021-02-23 23:37:18 -0800

    place holder for maps

--- client/src/components/ItemView.js
+++ client/src/components/ItemView.js
@@ -70,6 +70,8 @@ export default function ItemView() {
             <hr></hr>
         <MapContainer></MapContainer>
         </div>
+        <div class='embed-container'>
+          <iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2685.2329583238907!2d-122.33470708436639!3d47.69931457919095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5490140174d2fc9d%3A0xef595859431902b6!2sNorth%20Seattle%20College!5e0!3m2!1sen!2sus!4v1585028981234!5m2!1sen!2sus' ></iframe></div>
         </Grid>
         
 
