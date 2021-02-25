3c8f8ee120668b370001fe57c8833f99c1c0abdd
Abenezer T <abety8@outlook.com>
2021-02-24 20:43:24 -0800

    took out consloe.log

--- client/src/Auth0/auth0-provider-with-history.js
+++ client/src/Auth0/auth0-provider-with-history.js
@@ -9,6 +9,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
    
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+   
   const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
   console.log(audience)
   const history = useHistory();
