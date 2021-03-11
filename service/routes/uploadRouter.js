4fb5cdc739be3db22fb2ba105356fad2a037569d
Abenezer T <abety8@outlook.com>
2021-03-11 06:46:58 -0800

    deleted junk code

--- service/routes/uploadRouter.js
+++ service/routes/uploadRouter.js
@@ -9,15 +9,14 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage: storage});
-//const upload = multer({ dest: '../resources/uploads' });
+ 
 const uploadsRouter = express.Router();
 
 uploadsRouter.route('/')
   .post(upload.single('image'), (req, res, next) => {
     try {
       const featuredImage = req.file;
-      // by this point the file has been saved or an error has occurred.
-      // if req.file exists, the save was successful
+       
       if (!featuredImage) {
         res.status(400);
         res.send({ error: 'No file selected' });
