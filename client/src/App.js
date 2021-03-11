4f030527d087cb232af7103bb30f9b93b6fc423b
Abenezer T <abety8@outlook.com>
2021-03-11 07:01:15 -0800

    got ride of import causing error

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Nav from "./components/nav";
 import User_profile from "./components/user_profile";
 import Home from "./components/Home";
 //import ItemGroupBox from "./components/ItemGroupBox.js";
-import Chat from "./Chat";
+//import Chat from "./Chat";
 import { Dialog } from "@material-ui/core";
 import Dialogs from "./components/Dialogs";
 import FeatchProduct from "./components/FetchOneProd";
@@ -37,7 +37,7 @@ export function App() {
  
 
             <Route exact Path="/User_profile"> <User_profile/></Route>
-            <Route exact Path="/chat"> <Chat/></Route>
+            <Route exact Path="/chat"> </Route>
              
             
  
