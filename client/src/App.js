4d959a3065786d85060bff98c863d5923461e276
Abenezer T <abety8@outlook.com>
2021-03-24 20:18:21 -0700

    import changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import All from "./components/All";
+import All from "./components/Product/All";
 import Imgex from "./components/Imgex";
 import Nav from "./components/nav";
 import User_profile from "./components/user_profile";
 import Home from "./components/Home";
 import { Dialog } from "@material-ui/core";
-import Dialogs from "./components/Dialogs";
+import Dialogs from "./components/Message/Dialogs";
 //import FeatchProduct from "./components/ProductDetail/FetchOneProd";
 import FetchOneProd from "./components/ProductDetail/FetchOneProd";
 
 import { Auth0Provider } from "@auth0/auth0-react";
-import User from "./components/Userc";
-import CreatProduct from './components/CreatProduct'
+import User from "./components/Message/Userc";
+import CreatProduct from './components/Product/CreatProduct'
 import Profile from './components/Profile'
  
 
