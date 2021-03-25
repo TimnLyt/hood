b3b7d8ca39a6dd07e02f185db8b153246758c0b4
Abenezer T <abety8@outlook.com>
2021-03-24 20:16:21 -0700

    moved to a folder

---
+++ client/src/components/Message/Userc.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+//import Products from './FetchOneProd'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  makeStyles,
+  Badge,
+  Button,
+  Grid,
+  ButtonGroup,
+  Container
+} from "@material-ui/core";
+import { Link } from 'react-router-dom';
+import { blueGrey } from "@material-ui/core/colors";
+import Dialogs from "./Dialogs";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(1),
+    height:100,
+    maxWidth: 245,
+      
+    //marginRight:30,
+    backgroundColor:'blueGrey',
+  },
+  paper:{
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  Badge:{
+    
+    height: 10,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "black",
+     
+  },
+  button:{
+    height:28,
+    marginTop:10
+  }
+}));
+
+export default function User(props) {
+  const [contact, setcontact]=useState(false);
+  const classes = useStyles();
+   
+  return (
+    <>
+    <Container className={classes.root}>
+    <div className={classes.paper}> 
+        
+     
+          
+          <div className={classes.rbutton}>
+      <ButtonGroup color="primary" aria-label="large outlined primary button group">
+        <Button>Offer</Button>
+        <Button onClick={()=>setcontact(!contact)}>Question</Button>
+         
+      </ButtonGroup>
+      </div>
+           
+        
+      <div> 
+       
+      </div>
+    
+    </div>
+    {
+          contact?<Dialogs/>:null
+           }
+    </Container>  
+    </>
+  );
+}
